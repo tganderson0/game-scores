23dd768e014c08294187dcf31bb4d940a36a2288
Taylor Anderson <tganderson0@gmail.com>
2023-03-04 12:56:55 -0700

    added bottom margin

--- src/components/standard-layout.js
+++ src/components/standard-layout.js
@@ -2,7 +2,7 @@ import Header from './header';
 
 function Layout({ children }) {
     return (
-        <div className="container mx-auto content-center prose">
+        <div className="container mx-auto content-center prose mb-8">
             <Header />
               <div className="w-full flex flex-col items-center">
 
